ee38da8836aae4b6fb9311d25cc7ca86df319f89
Ramos <lenramos@deloitte.com>
2020-01-30 18:01:02 -0500

    begin login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Flutter/screens/login/login.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home:
+          LogIn(title: 'Log In'), //MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
